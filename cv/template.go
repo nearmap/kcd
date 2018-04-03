d6f765f2b223c1b03c9dbafe32e41bd4759a166c
Suneeta Mall <suneeta.mall@nearmap.com>
2018-04-03 16:02:35 +1000

    table fixes

--- cv/template.go
+++ cv/template.go
@@ -8,41 +8,15 @@ const cvListHTML = `
     <title>CVManager: Version list</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style id="intercom-stylesheet" type="text/css">
-        body {
-            padding: 0;
-            margin: 0;
-        }
-        h1, p, table {
-            font-size: 13px;
-            font-family: OpenSans,Lucida Grande,Lucida Sans Unicode,sans-serif;
-            font-weight: 100;
-            width: 100%;
-            color: #555;
-        }
-        h1 {
-            font-size: 16px;
-            width: 100%;
-            border-bottom: solid 1px #eee;
-            color: #7d7d7d;
-            padding: 3px;
-            margin: 0;
-        }
-        td, th {
-            text-align: left;
-            padding-bottom: 3px;
-        }
-        table {
-            margin: 15px;
-        }
+    body,h1{margin:0}body{padding:0}h1,p,table{font-size:13px;font-family:OpenSans,Lucida Grande,Lucida Sans Unicode,sans-serif;font-weight:100;width:100%;color:#555}h1{font-size:16px;width:100%;border-bottom:solid 1px #eee;color:#7d7d7d;padding:3px}td,th{text-align:left;padding-bottom:3px}table{margin:15px}
     </style>
 </head>
 <body>
-<main class="flex flex-column min-vh-100"><!-- use flex to separate the layouts into diff sections. -->
+<main>
     <h1>List of current version of CV managed deployments</h1>
-    <section class="flex-auto flex flex-column flex-row-ns">
-        <div class="pv4 pv2-ns ph2 overflow-y-scroll w-70-ns">
+    <section>
+        <div>
             <table >
-                <!-- Table header -->
                 <thead>
                 <tr>
                     <th scope="col">Namespace</th>
@@ -52,13 +26,6 @@ const cvListHTML = `
                     <th scope="col">Available pods/Status</th>
                 </tr>
                 </thead>
-                <!-- Table footer -->
-                <tfoot>
-                <tr>
-                    <td>END</td>
-                </tr>
-                </tfoot>
-                <!-- Table body -->
                 <tbody>
                 {{range .}}
                 <tr>
