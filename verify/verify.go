d55c5cc681e3e60360edec25424d4ac76df2016d
Simon Cochrane <simon.cochrane@users.noreply.github.com>
2018-04-25 16:13:55 +1000

    Add verify stub.

--- verify/verify.go
+++ verify/verify.go
@@ -1,8 +1,23 @@
 package verify
 
-type Verifier struct {
+type Verifier interface {
+	Verify() error
 }
 
-func NewVerifier() *Verifier {
-	return &Verifier{}
+type BasicVerifier struct {
+	containerImage string
+}
+
+// NewBasicVerifier runs a verification action by initializing a pod
+// with the given container immage and checking its exit code.
+// The Verify action passes if the image has a zero exit code.
+func NewBasicVerifier(containerImage string) *BasicVerifier {
+	return &BasicVerifier{
+		containerImage: containerImage,
+	}
+}
+
+func (bv *BasicVerifier) Verify() error {
+	// TODO:
+	return nil
 }
