00e2b7f0628740f2e8fdd1ca139d4f4cf033cea9
Steven Zhu <steven.zhu@nearmap.com>
2019-11-12 15:32:47 +1100

    remove default annotation value

--- service/controller.go
+++ service/controller.go
@@ -392,12 +392,6 @@ func (c *CVController) newKCDSyncDeployment(kcd *kcd1.KCD, version string) *apps
 		"app":        "registry-syncer",
 		"controller": kcd.Name,
 	}
-	annotations := map[string]string {
-		"sidecar.istio.io/inject": "false",
-	}
-	for k, v := range kcd.Annotations {
-		annotations[k] = v
-	}
 
 	return &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
@@ -422,7 +416,7 @@ func (c *CVController) newKCDSyncDeployment(kcd *kcd1.KCD, version string) *apps
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: labels,
-					Annotations: annotations,
+					Annotations: kcd.Annotations,
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
