9ec551fe22c41276a452aef087700b4b5766284a
dayuwang <dayuwang@wish.com>
2023-01-29 18:17:10 -0500

    remove the unrelated local change

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -2,12 +2,12 @@ package deploy
 
 import (
 	"github.com/golang/glog"
-	"github.com/pkg/errors"
 	kcd1 "github.com/wish/kcd/gok8s/apis/custom/v1"
 	"github.com/wish/kcd/gok8s/workload"
 	k8s "github.com/wish/kcd/gok8s/workload"
 	"github.com/wish/kcd/registry"
 	"github.com/wish/kcd/state"
+	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
